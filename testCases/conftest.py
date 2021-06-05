3b01f89b62dd8cface186e89d276338d47bcd0b0
tsrinu28852 <tsrinu28852@gmail.com>
2021-06-05 18:37:34 +0200

    enable logging

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
 import pytest
 
 @pytest.fixture()
@@ -21,6 +22,7 @@ def browser(request):  # This will return the Browser value to setup method
 ########### pytest HTML Report ################
 
 # It is hook for Adding Environment info to HTML Report
+
 def pytest_configure(config):
     config._metadata['Project Name'] = 'Selenium Project'
     config._metadata['Module Name'] = 'Login'
--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -1,12 +1,18 @@
 import logging
+import inspect
 
 class LogGen:
-
     @staticmethod
-    def loggen():
-        #logging.basicConfig(filename='automation.log',
-        logging.basicConfig(filename='.\\Logs\example.log', encoding='utf-8', level=logging.DEBUG)
-                            #format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-        logger=logging.getLogger()
-        logger.setLevel(logging.INFO)
+    def loggen(logLevel=logging.DEBUG):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        # By default, log all messages
+        logger.setLevel(logging.DEBUG)
+        fileHandler = logging.FileHandler(".//Logs//automation.log", mode='a')
+        fileHandler.setLevel(logLevel)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+                                      datefmt='%m/%d/%Y %I:%M:%S %p')
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
         return logger
+
